df9c2e3a2adb4e93640057621901d92fda8dc3b7
ElEnnero <159963896+ElEnnero@users.noreply.github.com>
2024-02-23 21:14:43 -0600

    Comenzando con una clase persona

---
+++ Proyecto1/src/proyecto1/Persona.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto1;
+
+/**
+ *
+ * @author Enner
+ */
+public class Persona {
+    private String nombre, apellidos;
+    private int edad;
+    private char sexo;
+    
+    
+    
+    
+    
+}
